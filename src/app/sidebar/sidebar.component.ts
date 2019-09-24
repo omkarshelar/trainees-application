13cfedc941ca0fc0bc11774eeca59f65e1b268f4
Shelar <799253@cognizant.com>
2019-09-24 12:08:22 +0530

    SideNav Done

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    console.log("Sidebar loaded");
+    
+  }
 
   ngOnInit() {
   }
