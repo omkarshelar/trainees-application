ebda274f0ec99912c28b1e7f906d2391a1a3cbaa
Shelar <799253@cognizant.com>
2019-09-24 14:46:31 +0530

    overview half done

--- src/app/components/trainee-overview/trainee-overview.component.ts
+++ src/app/components/trainee-overview/trainee-overview.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { StudentsService } from '../../services/students.service';
+import { Student } from '../../mock-students'
 
 @Component({
   selector: 'app-trainee-overview',
@@ -8,9 +10,14 @@ import { Router } from '@angular/router';
 })
 export class TraineeOverviewComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  students:Student[];
+  constructor(private studentServiceObj:StudentsService, private router:Router) { }
 
   ngOnInit() {
+    this.students = this.studentServiceObj.getStudents();
+  }
+  add() {
+    
   }
 
 }
